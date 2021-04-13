e704bc5ca145bf442d315365d7feec525f74a900
Michael <michael@Michaels-MacBook-Pro.local>
2021-04-13 13:47:26 +0800

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/hackust/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hackust
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:hackust/fitness_app_theme.dart';
+import 'package:hackust/fitness_app_home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: FitnessAppHomeScreen(),
+    );
+  }
+}
+
+class HexColor extends Color {
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll('#', '');
+    if (hexColor.length == 6) {
+      hexColor = 'FF' + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+}
